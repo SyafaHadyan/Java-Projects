360d93b76e854cde882dc5ea6e6466e20cf07b3e
Syafa Hadyan <syafahadyanrasendriya@gmail.com>
2023-03-03 15:55:52 +0800

    create settings.json, Variable/README.md and Variable.java

---
+++ Variable/src/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args) throws Exception {
+
+        long x = 3910795817039l;
+        String y = "Hello World";
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
