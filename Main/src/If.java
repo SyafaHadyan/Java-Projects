e726b3a0345f8da4ef4cb51875f4a3ca96d11221
Syafa Hadyan <syafahadyanrasendriya@gmail.com>
2023-03-10 16:54:35 +0800

    feat: create If.java

---
+++ Main/src/If.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class If {
+
+    public static void main(String[] args) throws Exception {
+        
+        Random Random = new Random();
+
+        int x = Random.nextInt();
+        boolean y = Random.nextBoolean();
+        double z = Random.nextDouble();
+        
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+    }
+}
